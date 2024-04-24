21712ae011b6187cd735333c183b98bf2dc471f1
rickelmeribeiro <rickasribeiro@gmail.com>
2024-04-24 14:00:00 -0300

    Col e Row

--- index.php
+++ index.php
@@ -33,6 +33,11 @@
     <div class="col-md-12">Adicionar</div>
 </div>
 
+<!-- TELA DE CAIXA -->
+<div class="row">
+    <div class="col-md-10">Menu Caixa</div>
+</div>
+
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
